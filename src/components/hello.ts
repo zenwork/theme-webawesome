1dc27107142df11672277b5ebae66e02db552e23
zework <florian@hehlen.name>
2026-05-12 23:07:55 -0400

    initial setup

--- src/components/hello.ts
+++ src/components/hello.ts
@@ -1,6 +1,7 @@
 import { html, LitElement } from 'lit'
 
 export class Hello extends LitElement {
+
   protected override render(): unknown {
     return html`
       HELLO WORLD! <wa-button variant="brand">HI!</wa-button>
