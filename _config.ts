1fee18bf6e0a13758ea08f6c332fb4266ff9061a
zework <florian@hehlen.name>
2026-05-17 16:35:30 -0400

    fix(toc): fixed root setup with site-toc

--- _config.ts
+++ _config.ts
@@ -1,19 +1,16 @@
 import lume from 'lume/mod.ts'
-import plugins from './plugins.ts'
 import theme from './mod.ts'
 
 const site = lume({ src: './src' })
 
-site.use(plugins())
-
 site.use(theme({
   siteToc: {
-    sectionsFromRoot: true,
+    sectionsFromRoot: false,
     rootLabel: 'Overview',
   },
   webawesome: {
     customPropertiesCssPath: '/styles/webawesome-theme.css',
   },
-               }))
+}))
 
 export default site
