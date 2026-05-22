ffb872d0579b8da4caf4ed117849308877557c37
zework <florian@hehlen.name>
2026-05-22 19:17:50 +0200

    feat(search): improve dropdown UX, focus, and result styling

--- plugins.ts
+++ plugins.ts
@@ -3,6 +3,7 @@ import basePath from 'lume/plugins/base_path.ts'
 import metas from 'lume/plugins/metas.ts'
 import nav from 'lume/plugins/nav.ts'
 import search from 'lume/plugins/search.ts'
+import pagefind from 'lume/plugins/pagefind.ts'
 import { Options as SitemapOptions, sitemap } from 'lume/plugins/sitemap.ts'
 import { favicon, Options as FaviconOptions } from 'lume/plugins/favicon.ts'
 import { merge } from 'lume/core/utils/object.ts'
@@ -532,6 +533,7 @@ export default function (userOptions?: Options) {
       .use(basePath())
       .use(nav())
       .use(search())
+      .use(pagefind({ ui: false }))
       .use(metas())
       .use(toc())
       .use(sitemap(options.sitemap))
